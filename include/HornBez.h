2951649dccf1282d1ddcb023002f51aa54dfbfbb
Andriamahenina Ramanantoanina <mahenina1994@gmail.com>
2024-01-31 15:38:44 +0100

    deleted

--- include/HornBez.h
+++
@@ -1,8 +0,0 @@
-#include <vector>
-#include "mpreal.h"
-
-using namespace std;
-using namespace mpfr;
-
-mpreal PolynomialHornBez(vector<mpreal> values, int n, mpreal t);
-mpreal RationalHornBez(vector<mpreal> values, vector<mpreal> weights, int n, mpreal t);
--- include/linearGeometric.h
+++
@@ -1,7 +0,0 @@
-#include <vector>
-#include "mpreal.h"
-
-using namespace std;
-using namespace mpfr;
-
-mpreal linearGeometric(vector<mpreal> values, vector<mpreal> weights, int n, mpreal t);
