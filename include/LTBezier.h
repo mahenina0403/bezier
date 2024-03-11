377020f783d2eb6bc069689a4f365af49881ccbc
Andriamahenina Ramanantoanina <mahenina1994@gmail.com>
2024-03-11 14:45:41 +0100

    stable algorithms

--- include/LTBezier.h
+++ include/LTBezier.h
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <eigen3/Eigen/Dense>
 #include <numbers>
+#include <stdexcept>
 
 using namespace std;
 
@@ -22,12 +23,11 @@ Type PolynomialDeCasteljau(vector<Type> values, int n, Type t){
 
 	for (int j=1; j<=n; j++){
 		for (int i=0; i<=n-j; i++)
-			values[i] = t*values[i] + s*values[i+1];
+			values[i] = s*values[i] + t*values[i+1];
 	}
 
 	return values[0];
 }
-
 
 template <class Type>
 Type RationalDeCasteljau(vector<double> values, vector<double> weights, int n, double t){
@@ -41,8 +41,8 @@ Type RationalDeCasteljau(vector<double> values, vector<double> weights, int n, d
 
     for (int j=1; j<=n; j++){
 		for (int i=0; i<=n-j; i++){
-			num[i] = (Type)t*num[i] + s*num[i+1];
-			den[i] = (Type)t*den[i] + s*den[i+1];
+			num[i] = (Type)s*num[i] + t*num[i+1];
+			den[i] = (Type)s*den[i] + t*den[i+1];
 		}
 	}
 
@@ -63,8 +63,8 @@ Type FarinRationalDeCasteljau(vector<double> values, vector<double> weights, int
 	Type c1, c2, tmp;
 	for (int j=1; j<=n; j++){
 		for (int i=0; i<=n-j; i++) {
-			c1 = t*w[i];
-			c2 = s*w[i+1];
+			c1 = s*w[i];
+			c2 = t*w[i+1];
 			tmp = c1 + c2;
 			c1 = c1/tmp;
 			c2 = 1 - c1;
@@ -76,85 +76,6 @@ Type FarinRationalDeCasteljau(vector<double> values, vector<double> weights, int
 	return R[0];
 }
 
-//BARYCENTRIC
-template <class Type>
-vector<Type> get_homogeneous_values(vector<double> values, vector<double> weights, int n){
-    const Type pi = Pi<Type>();
-    vector<Type> Values(n+1);
-    for (int i=0; i<=n; i++)
-        Values[i] = (Type) values[i] * weights[i];
-
-    Type t;
-    vector<Type> N(n+1);
-    for (int i=0; i<=n; i++){
-        t = cos(i/(Type)n * pi);
-        t = (t+1)/2;
-        N[i] = PolynomialDeCasteljau<Type>(Values, n, t);
-    }
-
-    return N;
-}
-
-template <class Type>
-vector<Type> get_barycentric_weights(vector<double> weights, int n){
-    const Type pi = Pi<Type>();
-    vector<Type> W(n+1);
-    for(int i=0; i<=n; i++)
-        W[i] = (Type) weights[i];
-
-    vector<Type> N(n+1);
-    Type t;
-    Type lagrange_weight;
-    int sgn = 1;
-
-    for (int i=0; i<=n; i++){
-        t = cos(i/(Type)n * pi);
-        t = (t+1)/2;
-        if (i==0 || i==n)
-            lagrange_weight = 0.5;
-        else
-            lagrange_weight = 1;
-
-        N[i] = sgn * lagrange_weight * PolynomialDeCasteljau<Type>(W, n, t);
-        sgn = -sgn;
-    }
-
-    return N;
-}
-
-template <class Type>
-void get_barycentric_data(vector<Type>& values, vector<Type> weights, int n){
-    int sgn = 1;
-    Type lagrange_weight;
-    for (int i=0; i<=n; i++){
-        if (i==0 || i==n)
-            lagrange_weight = 0.5;
-        else
-            lagrange_weight = 1;
-
-        values[i] *= sgn * lagrange_weight / weights[i];
-        sgn = -sgn;
-    }
-}
-
-template <class Type>
-Type barycentric(vector<Type> V, vector<Type> W, int n, double t){
-    const Type pi = Pi<Type>();
-
-    //evaluation
-    Type N = 0;
-    Type D = 0;
-    for (int i=0; i<=n; i++) {
-        Type r = (Type)t - (cos(i/(Type)n * pi) + 1) / 2;
-        if (r == 0)
-            return V[i];
-        r = W[i]/r;
-        N += r * V[i];
-        D += r;
-    }
-
-    return N/D;
-}
 
 //HORNBEZ
 template <class Type>
@@ -162,22 +83,53 @@ Type RationalHornBez(vector<double> values, vector<double> weights, int n, doubl
 	Type s = 1 -(Type) t;
 	Type tk = 1;
 	Type b = 1;
-	Type N = (Type) weights[n]*values[n]*s;
-	Type D = (Type) weights[n]*s;
+	Type tmp = (Type) weights[0]*s;
+	Type N = tmp*(Type)values[0]*s;
+	Type D = tmp;
 
 	for (int i=1; i<n; i++){
 		tk *= t;
 		b *= (n+1-i)/(Type)i;
-		N = (N+tk*b*weights[n-i]*values[n-i])*s;
-		D = (D+tk*b*weights[n-i])*s;
+		tmp = tk*b*(Type)weights[i];
+		N = (N+tmp*(Type)values[i])*s;
+		D = (D+tmp)*s;
 	}
-	if (n > 0){
-		tk *= t;
-		N += tk*weights[0]*values[0];
-		D += tk*weights[0];
-	}
+	// if (n > 0){
+	tk *= t;
+	tmp = tk*(Type)weights[n];
+	N += tmp*(Type)values[n];
+	D += tmp;
+	// }
 
 	return N/D;
+}
+
+//LINEAR GEOMETRIC
+template <class Type>
+Type linearGeometric(vector<double> values, vector<double> weights, int n, double t){
+	Type h = 1;
+	Type u = 1 - (Type)t;
+	unsigned int n1 = n + 1;
+	Type R = values[0];
+
+	if(t <= 0.5) {
+		u = t / u;
+		for(int k=1; k<=n; k++){
+			h *= u * (n1-k) * weights[k];
+			h /= (k * (Type)weights[k-1] + h);
+			R = (1 - (Type)h) * R + h * values[k];
+		}
+	}
+	else {
+		u /= t;
+		for(int k = 1; k <= n; k++) {
+			h *= (n1-k) * (Type)weights[k];
+			h /= (k * u * weights[k-1] + h);
+			R = (1 - (Type)h) * R + h * values[k];
+		}
+	}
+
+	return R;
 }
 
 //LADER
@@ -189,57 +141,32 @@ Type RationalLader(vector<double> values, vector<double> weights, int n, double 
     Type N;
     Type D;
 
-	if (t < 1/2){
+	if (t < 0.5){
 	    s = t / (1 - (Type)t);
-		N = (Type) weights[n]*values[n];
-		D = (Type) weights[n];
-		for (int i=1; i<=n; i++){
-			sk *= s * (n-i+1) / (Type)i;
-			N += sk*weights[n-i]*values[n-i];
-			D += sk*weights[n-i];
-		}
-	}
-	else{
-	    s = (1 - (Type)t)/t;
 		N = (Type) weights[0]*values[0];
 		D = (Type) weights[0];
 		for (int i=1; i<=n; i++){
 			sk *= s * (n-i+1) / (Type)i;
-			N += sk*weights[i]*values[i];
-			D += sk*weights[i];
+			auto tmp = sk*weights[i];
+			N += tmp*values[i];
+			D += tmp;
+		}
+	}
+	else{
+	    s = (1 - (Type)t)/t;
+		N = (Type) weights[n]*values[n];
+		D = (Type) weights[n];
+		for (int i=1; i<=n; i++){
+			sk *= s * (n-i+1) / (Type)i;
+			auto tmp = sk*weights[n-i];
+			N += tmp*values[n-i];
+			D += tmp;
 		}
 	}
 
 	return N/D;
 }
 
-//LINEAR GEOMETRIC
-template <class Type>
-Type linearGeometric(vector<double> values, vector<double> weights, int n, double t){
-	Type h = 1;
-	Type u = 1 - (Type)t;
-	unsigned int n1 = n + 1;
-	Type R = values[n];
-
-	if(t <= 0.5) {
-		u = t / u;
-		for(int k=1; k<=n; k++){
-			h *= u * (n1-k) * weights[n-k];
-			h /= (k * (Type)weights[n-k+1] + h);
-			R = (1 - (Type)h) * R + h * values[n-k];
-		}
-	}
-	else {
-		u /= t;
-		for(int k = 1; k <= n; k++) {
-			h *= (n1-k) * (Type)weights[n-k];
-			h /= (k * u * weights[n-k+1] + h);
-			R = (1 - (Type)h) * R + h * values[n-k];
-		}
-	}
-
-	return R;
-}
 
 //VS
 template <class Type>
@@ -249,24 +176,26 @@ Type RationalVS(vector<double> values, vector<double> weights, int n, double t){
 	Type N;
 	Type D;
 
-	if (t < 1/2){
+	if (t < 0.5){
 		s  = t / (1 - (Type)t);
-		N = (Type)weights[0]*values[0];
-		D = (Type)weights[0];
-		for (int i=1; i<=n; i++){
-			b *= (n-i+1) / (Type)i;
-			N = N*s + (Type)weights[i]*values[i]*b;
-			D = D*s + (Type)weights[i]*b;
-		}
-	}
-	else{
-		s  = (1 - (Type)t) / t;
 		N = (Type)weights[n]*values[n];
 		D = (Type)weights[n];
 		for (int i=1; i<=n; i++){
 			b *= (n-i+1) / (Type)i;
-			N = N*s + (Type)weights[n-i]*values[n-i]*b;
-			D = D*s + (Type)weights[n-i]*b;
+			auto tmp = (Type)weights[n-i]*b;
+			N = N*s + tmp*values[n-i];
+			D = D*s + tmp;
+		}
+	}
+	else{
+		s  = (1 - (Type)t) / t;
+		N = (Type)weights[0]*values[0];
+		D = (Type)weights[0];
+		for (int i=1; i<=n; i++){
+			b *= (n-i+1) / (Type)i;
+			auto tmp = (Type)weights[i]*b;
+			N = N*s + tmp*values[i];
+			D = D*s + tmp;
 		}
 	}
 
@@ -313,6 +242,97 @@ Type polynomialWangBall(vector<Type> values, int n, double t){
 }
 
 template <class Type>
+Type at_k(vector<Type> P, int k){
+	int n = P.size()-1;
+	int i;
+
+	Type A = 0;
+	Type B = 0;
+
+	Type floor_n2 = floor((Type)n/2);
+	Type ceil_n2 = ceil((Type)n/2);
+
+	int start = n-k+1;
+	int end = n-k;
+	if (k==floor_n2)
+		start = k+2;
+	if (k==ceil_n2)
+		end = k-2;
+
+	if (k <= floor_n2){
+		for (i=0; i<=k-1; i++)
+			A = A + pow((Type)2,i) * n_choose_k<Type>(n-2-2*i, k-i) * P[i];
+		for (i=start; i<=n;i++)
+			B = B + pow((Type)2,n-i) * n_choose_k<Type>(2*i-2-n,i-k) * P[i];
+	}
+	// else if (k ==floor_n2){
+	// 	for (i=0; i<k; i++)
+	// 		A = A + pow((Type)2,i) * n_choose_k<Type>(n-2-2*i, k-i) * P[i];
+	// 	for (i=k+2; i<n+1;i++)
+	// 		B = B + pow((Type)2,n-i) * n_choose_k<Type>(2*i-2-n,i-k) * P[i];
+	// }
+	else if (k >= ceil_n2){
+		for (i=0; i<=end; i++)
+			A = A + pow((Type)2,i) * n_choose_k<Type>(n-2-2*i,k-i) * P[i];
+		for (i=k+1; i<=n; i++)
+			B = B + pow((Type)2,n-i) * n_choose_k<Type>(2*i-2-n,i-k) * P[i];
+	}
+
+	return (A+B)/n_choose_k<Type>(n,k);
+}
+
+
+template <class Type>
+vector<vector<Type>> convert_to_wang_ball_stable(vector<double> values, vector<double> weights, int n){
+	typedef Eigen::Matrix<Type, Eigen::Dynamic, Eigen::Dynamic> Matrix;
+
+	vector<Type> Values(n+1);
+	vector<Type> Weights(n+1);
+    for (int i=0; i<=n; i++){
+        Values[i] = (Type) values[i] * weights[i];
+        Weights[i] = (Type) weights[i];
+    }
+
+    Type floor_n2 = floor((Type)n/2);
+	Type ceil_n2 = ceil((Type)n/2);
+
+	vector<vector<Type>> R(2);
+	R[0].resize(n+1);
+	R[1].resize(n+1);
+
+	Type binomial = 1;
+	R[0][0] = Values[0];
+	R[1][0] = Weights[0];
+	R[0][n] = Values[n];
+	R[1][n] = Weights[n];
+	int k = 1;
+	while(k <= n-k){
+		binomial = binomial * (n-k+1)/(Type)k;
+		Type constant = 0;
+		constant = binomial / pow((Type)2,k);
+		R[0][k] = (Values[k]-at_k<Type>(R[0],k)) * constant;
+		R[1][k] = (Weights[k]-at_k<Type>(R[1],k)) * constant;
+
+		int K = n-k;
+
+		if (K==k)
+			break;
+
+		constant = binomial / pow((Type)2,n-K);
+		R[0][K] = (Values[K]-at_k<Type>(R[0],K)) * constant;
+		R[1][K] = (Weights[K]-at_k<Type>(R[1],K)) * constant;
+
+		// cout << k << ", " << K << endl;
+
+		k++;
+	}
+	for (int i=0; i<=n; i++)
+        R[0][i] = R[0][i]/R[1][i];
+
+	return R;
+}
+
+template <class Type>
 vector<vector<Type>> convert_to_wang_ball(vector<double> values, vector<double> weights, int n){
 	typedef Eigen::Matrix<Type, Eigen::Dynamic, Eigen::Dynamic> Matrix;
 
@@ -324,8 +344,8 @@ vector<vector<Type>> convert_to_wang_ball(vector<double> values, vector<double> 
 
 	Matrix data(2,n+1);
 	for (i=0; i<= n; i++){
-		data(0,i) = (Type)values[n-i]*weights[n-i];
-		data(1,i) = (Type)weights[n-i];
+		data(0,i) = (Type)values[i]*weights[i];
+		data(1,i) = (Type)weights[i];
 	}
 
 	Matrix M(n+1,n+1);
@@ -360,7 +380,7 @@ vector<vector<Type>> convert_to_wang_ball(vector<double> values, vector<double> 
 	data *= M;
 
 	for (i=0; i<=n; i++){
-		R[0][i] = data(0,i);
+		R[0][i] = data(0,i)/data(1,i);
 		R[1][i] = data(1,i);
 	}
 
@@ -375,5 +395,386 @@ Type rationalWangBall(vector<vector<Type>> R, int n, double t){
 
 	return numerator/denominator;
 }
+
+// template <class Type>
+// Type rationalWangBall_2(vector<vector<Type>> R, double t){
+
+	// int n = R[0].size()-1;
+	// vector<Type> N(n+1);
+	// vector<Type> D(n+1);
+	
+	// N = R[0];
+	// D = R[1];
+
+	// Type t1 = 1 - (Type)t;
+	// Type a,b,c,d,e,f,wq,wr,ww;
+	// int k = n;
+	// int k1,k2;
+	// int shift;
+	// int n_jump = 0;
+	// while (k > 2){
+	// 	if (k%2==1){
+	// 		k1 = (int)(n-1)/2;
+	// 		k2 = (int)(n+1)/2;
+	// 		// if (n_jump==0)
+	// 		// 	n_jump = k2;
+	// 		a = t1*D[k1];
+	// 		b = t*D[k2];
+	// 		wq = a+b;
+	// 		D[k1] = wq;
+	// 		N[k1] = (a*N[k1]+b*N[k2])/wq;
+	// 		shift = k2;
+	// 		// n_jump++;
+	// 		// cout << k1 << ", " << n_jump << endl;
+	// 	}else{
+	// 		k2 = (int)k/2;
+	// 		// if (n_jump==0)
+	// 		// 	n_jump = k2+1;
+	// 		a = t1*D[k2-1];
+	// 		b = t*D[k2];
+	// 		wq = a+b;
+	// 		D[k2-1] = wq;
+	// 		N[k2-1] = (a*N[k2-1]+b*N[k2])/wq;
+			
+	// 		a = t1*D[k2];
+	// 		b = t*D[n_jump];
+	// 		wq = a+b;
+	// 		D[k2] = wq;
+	// 		N[k2] = (a*N[k2]+b*N[n_jump])/wq;
+	// 		shift = k2+1;
+	// 		// cout << k2 << ", " << n_jump << endl;
+	// 	}
+	// 	// for (int i=shift; i<k; i++){
+	// 	// 	D[i] = D[i+1];
+	// 	// 	N[i] = N[i+1];
+	// 	// }
+	// 	n_jump++;
+	// 	k--;
+
+
+	// }
+
+	// 	a = t1*D[0];
+	// 	b = t*D[1];
+	// 	c = t1*D[1];
+	// 	d = t*D[n];
+
+	// 	wq = a + b;
+	// 	wr = c + d;
+
+	// 	e = t1*wq;
+	// 	f = t*wr;
+	// 	ww = e + f;
+
+	// 	Type Q = (a*N[0] + b*N[1])/wq;
+	// 	Type V = (c*N[1] + d*N[n])/wr;
+	// 	Q = (e*Q + f*V)/ww;
+
+	// return Q;
+	
+// }
+
+template <class Type>
+Type rationalWangBall_2(vector<vector<Type>> R, double t){
+
+	int n = R[0].size()-1;
+
+	if (n < 2){
+		cout << "Error: Wang-Ball curve needs 3 points at least" << endl;
+		exit(1);
+	}
+	vector<Type> N(n+1);
+	vector<Type> D(n+1);
+	for(int i=0;i<n+1;i++){
+		N[i] = R[0][i];
+		D[i] = R[1][i];
+	}
+
+	Type t1 = 1 - (Type)t;
+
+	int k = n;
+	int jump = 0;
+	while (k>2){
+		if (k%2==1){
+			int n1 = (k-1)/2;
+			int n2 = (k+1)/2;
+
+			Type a = t1*D[n1];
+			Type b = t*D[n2];
+			if (jump == 0)
+				jump = n2+1;
+			
+			D[n1] = a+b;
+			N[n1] = (a*N[n1]+b*N[n2])/D[n1];
+			
+		}
+		else{
+			int n2 = k/2;
+			if (jump==0)
+				jump=n2+1;
+
+			Type a = t1*D[n2-1];
+			Type b = t*D[n2];
+			Type c = t1*D[n2];
+			Type d = t*D[jump];
+	
+			D[n2-1] = a+b;
+			D[n2] = c+d;
+	
+			N[n2-1] = (a*N[n2-1]+b*N[n2])/D[n2-1];
+			N[n2] = (c*N[n2]+d*N[jump])/D[n2];
+
+			jump++;
+			
+		}
+		k--;
+	}
+
+	Type a = t1*D[0];
+	Type b = t*D[1];
+	Type c = t1*D[1];
+	Type d = t*D[n];
+
+	Type wq = a + b;
+	Type wr = c + d;
+
+	Type e = t1*wq;
+	Type f = t*wr;
+	Type ww = e + f;
+
+	Type Q = (a*N[0] + b*N[1])/wq;
+	Type V = (c*N[1] + d*N[n])/wr;
+	Type W = (e*Q + f*V)/ww;
+
+	return W;
+	
+}
+
+
+// template <class Type>
+// Type rationalWangBall_2(vector<vector<Type>> R, double t){
+
+// 	int n = R[0].size()-1;
+
+// 	Type t1 = 1 - (Type)t;
+
+// 	if (n==2){
+// 		Type a = t1*R[1][0];
+// 		Type b = t*R[1][1];
+// 		Type c = t1*R[1][1];
+// 		Type d = t*R[1][2];
+
+// 		Type wq = a + b;
+// 		Type wr = c + d;
+
+// 		Type e = t1*wq;
+// 		Type f = t*wr;
+// 		Type ww = e + f;
+
+// 		Type Q = (a*R[0][0] + b*R[0][1])/wq;
+// 		Type V = (c*R[0][1] + d*R[0][2])/wr;
+// 		Type W = (e*Q + f*V)/ww;
+
+// 		return W;
+// 	}
+
+// 	vector<vector<Type>> S(2);
+// 	S[0].resize(n);
+// 	S[1].resize(n);
+
+// 	if (n%2==1){
+// 		int n1 = (n-1)/2;
+// 		int n2 = (n+1)/2;
+
+// 		for (int i=0; i<= (n-3)/2; i++){
+// 			S[0][i] = R[0][i];
+// 			S[1][i] = R[1][i];
+// 		}
+		
+// 		Type a = t1*R[1][n1];
+// 		Type b = t*R[1][n2];
+
+// 		S[1][n1] = a+b;
+// 		S[0][n1] = (a*R[0][n1]+b*R[0][n2])/S[1][n1];
+
+// 		for (int i=n2; i<= n-1; i++){
+// 			S[0][i] = R[0][i+1];
+// 			S[1][i] = R[1][i+1];
+// 		}
+// 	}
+// 	else{
+// 		int n2 = n/2;
+
+// 		for (int i=0; i < n/2; i++){
+// 			S[0][i] = R[0][i];
+// 			S[1][i] = R[1][i];
+// 		}
+		
+// 		Type a = t1*R[1][n2-1];
+// 		Type b = t*R[1][n2];
+// 		Type c = t1*R[1][n2];
+// 		Type d = t*R[1][n2+1];
+
+// 		S[1][n2-1] = a+b;
+// 		S[1][n2] = c+d;
+
+// 		S[0][n2-1] = (a*R[0][n2-1]+b*R[0][n2])/S[1][n2-1];
+// 		S[0][n2] = (c*R[0][n2]+d*R[0][n2+1])/S[1][n2];
+
+// 		for (int i=n2+1; i<= n-1; i++){
+// 			S[0][i] = R[0][i+1];
+// 			S[1][i] = R[1][i+1];
+// 		}
+// 	}
+//     return rationalWangBall_2<Type>(S, t);
+	
+// }
+
+//BARYCENTRIC
+template <class Type>
+vector<vector<Type>> get_data(vector<double> values, vector<double> weights, int n){
+
+	// const Type pi = Pi<Type>();
+
+    vector<Type> Values(n+1);
+    vector<Type> W(n+1);
+    vector<Type> ones(n+1);
+    for (int i=0; i<=n; i++){
+        Values[i] = (Type) values[i] * weights[i];
+        W[i] = (Type) weights[i];
+        ones[i]=1;
+    }
+
+    Type t;
+    vector<vector<Type>> N(2);
+    N[0].resize(n+1);
+    N[1].resize(n+1);
+
+    Type binomial = 1;
+    Type lagrange_weight;
+    for (int i=0; i<=n; i++){
+        t = i/(Type)n;
+        // t = (cos(i/(Type)n * pi)+1)/2;
+        
+        // if (i==0 || i==n)
+        //     lagrange_weight = 0.5;
+        // else
+        //     lagrange_weight = 1;
+
+        Type zf = RationalLader<Type>(Values, ones, n, t);
+        Type z = RationalLader<Type>(W, ones, n, t);
+        
+
+        // N[0][i] = lagrange_weight * zf;
+        // N[1][i] =  lagrange_weight * z;
+        
+        N[0][i] = binomial * zf;
+        N[1][i] =  binomial * z;
+        binomial *= (n+1-(i+1))/(Type)(i+1);
+    }
+
+    return N;
+}
+
+//BARYCENTRIC
+
+template <class Type>
+vector<Type> get_nodes(int n){
+	const Type pi = Pi<Type>();
+	vector<Type> T(n+1);
+	for (int i=0; i<=n; i++)
+		// T[i] = (cos(i/(Type)n * pi)+1)/2;
+		T[i] = i/n;
+
+	return T;
+}
+
+// template <class Type>
+// vector<Type> get_homogeneous_values(vector<double> values, vector<double> weights, vector<Type> T, int n){
+//     // const Type pi = Pi<Type>();
+//     vector<Type> Values(n+1);
+//     for (int i=0; i<=n; i++)
+//         Values[i] = (Type) values[i] * weights[i];
+
+//     Type t;
+//     vector<Type> N(n+1);
+//     for (int i=0; i<=n; i++){
+//         // t = cos(i/(Type)n * pi);
+//         // t = (t+1)/2;
+//         t = T[i];
+//         N[i] = PolynomialDeCasteljau<Type>(Values, n, t);
+//     }
+//     return N;
+// }
+
+// template <class Type>
+// vector<Type> get_barycentric_weights(vector<double> weights, vector<Type> T, int n){
+//     // const Type pi = Pi<Type>();
+//     vector<Type> W(n+1);
+//     for(int i=0; i<=n; i++)
+//         W[i] = (Type) weights[i];
+
+//     vector<Type> N(n+1);
+//     Type t;
+//     Type lagrange_weight;
+//     int sgn = 1;
+
+//     for (int i=0; i<=n; i++){
+//         // t = cos(i/(Type)n * pi);
+//         // t = (t+1)/2;
+//         t = T[i];
+//         if (i==0 || i==n)
+//             lagrange_weight = 0.5;
+//         else
+//             lagrange_weight = 1;
+
+//         N[i] = sgn * lagrange_weight * PolynomialDeCasteljau<Type>(W, n, t);
+//         sgn = -sgn;
+//     }
+
+//     return N;
+// }
+
+// template <class Type>
+// void get_barycentric_data(vector<Type>& values, vector<Type> weights, int n){
+//     int sgn = 1;
+//     Type lagrange_weight;
+//     for (int i=0; i<=n; i++){
+//         if (i==0 || i==n)
+//             lagrange_weight = 0.5;
+//         else
+//             lagrange_weight = 1;
+
+//         values[i] *= sgn * lagrange_weight / weights[i];
+//         sgn = -sgn;
+//     }
+// }
+
+template <class Type>
+Type barycentric(vector<Type> V, vector<Type> W, int n, double t){
+    // const Type pi = Pi<Type>();
+
+    //evaluation
+    Type N = 0;
+    Type D = 0;
+    Type r;
+    int sgn = 1;
+    for (int i=0; i<=n; i++) {
+        // r = t - (cos(i/(Type)n * pi) + 1) / 2;
+        r = (Type)t-i/(Type)n;
+
+        // Type r = (Type)t - T[i];
+        if (r == 0)
+            return V[i]/W[i];
+        r = sgn / r;
+        N += r * V[i];
+        D += r * W[i];
+        sgn = -sgn;
+    }
+    // for (int i=0)
+    // cout << D << endl;
+    return N/D;
+}
+
 
 #endif // LTBEZIER_H_INCLUDED
--- src/main.cpp
+++ src/main.cpp
@@ -2,56 +2,190 @@
 
 #include <iostream>
 #include <fstream>
+#include <ctime>
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <ctime>
+#include <iomanip>
+
 #include "LTBezier.h"
 #include "mpreal.h"
 
 using namespace std;
 using mpfr::mpreal;
 
-int main(){
+void relative_error(vector<double> f, vector<double> beta, int n, double t){
+    mpreal absolute = RationalDeCasteljau<mpreal>(f,beta,n,t);
+    double decast = RationalDeCasteljau<double>(f,beta,n,t);
+    double farin = FarinRationalDeCasteljau<double>(f,beta,n,t);
+    double vs = RationalVS<double>(f,beta,n,t);
+    double hornBez = RationalHornBez<double>(f,beta,n,t);
+    double lader = RationalLader<double>(f,beta,n,t);
+    double linearGeo = linearGeometric<double>(f,beta,n,t);
+
+    // vector<double> T = get_nodes<double>(n);
+    // vector<double> V = get_homogeneous_values<double>(f,beta,T,n);
+    // vector<double> W = get_barycentric_weights<double>(beta,T,n);
+    // get_barycentric_data<double>(V,W,n);
+    // double bary = barycentric<double>(V,W,T,n,t);
+
+    vector<vector<double>> D = get_data<double>(f,beta,n);
+    // for(int i=0; i<=n; i++){
+    //     cout << D[0][i] << ", " << D[1][i] << endl;
+    // }
+    double bary = barycentric<double>(D[0],D[1],n,t);
+
+    vector<vector<double>>  M = convert_to_wang_ball_stable<double>(f,beta,n);
+    double wangBall = rationalWangBall_2<double>(M,t);
+
+    cout.precision(5);
+    cout << "DeCasteljau:      " << absolute - (mpreal)decast << endl;
+    cout << "FarinDeCasteljau: " << absolute - (mpreal)farin << endl;
+    cout << "VS:               " << absolute - (mpreal)vs << endl;
+    cout << "HornBez:          " << absolute - (mpreal)hornBez << endl;
+    cout << "Lader:            " << absolute - (mpreal)lader << endl;
+    cout << "linearGeometric:  " << absolute - (mpreal)linearGeo << endl;
+    cout << "barycentric:      " << absolute - (mpreal)bary << endl;
+    cout << "WangBall:         " << absolute - (mpreal)wangBall << endl;
+}
+
+void efficiency_comparison(vector<double> f, vector<double> beta, int n, double t, int Sample=1000){
+    // int Sample = 1000;
+    cout.precision(10);
+    
+    clock_t startTime = clock();
+    for (int i=0; i<=Sample; i++){
+        t = i / (double) Sample;
+        RationalDeCasteljau<double>(f,beta,n,t);
+    }
+    clock_t endTime = clock();
+    double t1 = (endTime-startTime) / (double) CLOCKS_PER_SEC;
+    cout << "DeCasteljau:      " << t1 << "." << endl;
+
+    startTime = clock();
+    for (int i=0; i<=Sample; i++){
+        t = i / (double) Sample;
+        FarinRationalDeCasteljau<double>(f,beta,n,t);
+    }
+    endTime = clock();
+    t1 = (endTime-startTime) / (double) CLOCKS_PER_SEC;
+    cout << "FarinDeCasteljau: " << t1 << "." << endl;
+    
+    startTime = clock();
+    for (int i=0; i<=Sample; i++){
+        t = i / (double) Sample;
+        RationalVS<double>(f,beta,n,t);
+    }
+    endTime = clock();
+    t1 = (endTime-startTime) / (double) CLOCKS_PER_SEC;
+    cout << "VS:               " << t1 << "." << endl;
+
+    startTime = clock();
+    for (int i=0; i<=Sample; i++){
+        t = i / (double) Sample;
+        RationalHornBez<double>(f,beta,n,t);
+    }
+    endTime = clock();
+    t1 = (endTime-startTime) / (double) CLOCKS_PER_SEC;
+    cout << "HornBez:          " << t1 << "." << endl;
+
+    startTime = clock();
+    for (int i=0; i<=Sample; i++){
+        t = i / (double) Sample;
+        RationalLader<double>(f,beta,n,t);
+    }
+    endTime = clock();
+    t1 = (endTime-startTime) / (double) CLOCKS_PER_SEC;
+    cout << "Lader:            " << t1 << "." << endl;
+
+    startTime = clock();
+    for (int i=0; i<=Sample; i++){
+        t = i / (double) Sample;
+        linearGeometric<double>(f,beta,n,t);
+    }
+    endTime = clock();
+    t1 = (endTime-startTime) / (double) CLOCKS_PER_SEC;
+    cout << "linearGeometric:  " << t1 << "." << endl;
+
+    startTime = clock();
+    vector<vector<double>> M = convert_to_wang_ball_stable<double>(f,beta,n);
+    for (int i=0; i<=Sample; i++){
+        t = i / (double) Sample;
+        rationalWangBall_2<double>(M,t);
+    }
+    endTime = clock();
+    t1 = (endTime-startTime) / (double) CLOCKS_PER_SEC;
+    cout << "WangBall:         " << t1 << "." << endl;
+
+    
+    // vector<double> T = get_nodes<double>(n);
+    // vector<double> V = get_homogeneous_values<double>(f,beta,T,n);
+    // vector<double> W = get_barycentric_weights<double>(beta,T,n);
+    // get_barycentric_data<double>(V,W,n);
+    vector<double> T(n+1);
+    // for (int i=0; i<=n; i++)
+    //     T[i] = i/n;
+    
+    // T = get_nodes<double>(n);
+
+    startTime = clock();
+    vector<vector<double>> D = get_data<double>(f,beta,n);
+    vector<double> V = D[0];
+    vector<double> W = D[1];
+    
+    for (int i=0; i<=Sample; i++){
+        t = i / (double) Sample;
+        barycentric<double>(V,W,n,t);
+    }
+    endTime = clock();
+    t1 = (endTime-startTime) / (double) CLOCKS_PER_SEC;
+    cout << "barycentric:      " << t1 << "." << endl;
+}
+
+int main(int argc, char* argv[]){
     int my_mpreal_precision = 1024;
     mpreal::set_default_prec(my_mpreal_precision);
 
     cout.precision(200);
 
-    double t = 0.99;
-    int n = 3;
+    double t = 0.5;
+    int n = 20;
 
 	vector<double> f(n+1);
 	vector<double> beta(n+1);
 
-	f[0] = 0.1;
-	f[1] = 0.00000002;
-	f[2] = 0.0000001;
-	f[3] = 0.3;
+    for(int i=0; i<=n; i++){
+        f[i] = i*pow(-1,i);
+        beta[i] = 1;
+    }
+    beta[0] = 2;
+    beta[n] = 2;
 
-	beta[0] = 1;
-	beta[1] = 0.1;
-	beta[2] = 0.3;
-	beta[3] = 1;
+    if (argc == 2 && strcmp(argv[1],"-r")==0)
+        relative_error(f,beta,n,t);
+    else if (argc == 3 && strcmp(argv[1],"-r")==0){
+        t = atof(argv[2]);
+        relative_error(f,beta,n,t);
+    }
+    else if (argc == 2 && strcmp(argv[1],"-e")==0)
+        efficiency_comparison(f,beta,n,t);
+    else if (argc == 3 && strcmp(argv[1],"-e")==0){
+        efficiency_comparison(f,beta,n,t,atoi(argv[2]));
+    }
+    else{
+        cout << "Command:" << endl;
+        cout << setw(25) << left << "./compare -e";
+        cout << "compare the efficiency with 1000 sample" << endl;
+        cout << setw(25) << "./commpare -e [sample]";
+        cout << "compare the efficiency with a user defined number of sample" << endl;
+        cout << setw(25) << "./compare -r";
+        cout << "ompare the relative error at t = 0.5" << endl;
+        cout << setw(25) << "./compare -r [t]";
+        cout << "compare the relative error at a user defined parameter t" << endl;
+    }
 
-    mpreal deCast = RationalDeCasteljau<mpreal>(f,beta,n,t);
-    mpreal farin = FarinRationalDeCasteljau<mpreal>(f,beta,n,t);
-    mpreal vs = RationalVS<mpreal>(f,beta,n,t);
-    mpreal hornBez = RationalHornBez<mpreal>(f,beta,n,t);
-    mpreal lader = RationalLader<mpreal>(f,beta,n,t);
-    mpreal linearGeo = linearGeometric<mpreal>(f,beta,n,t);
-
-    vector<mpreal> V = get_homogeneous_values<mpreal>(f,beta,n);
-    vector<mpreal> W = get_barycentric_weights<mpreal>(beta,n);
-    get_barycentric_data<mpreal>(V,W,n);
-    mpreal bary = barycentric<mpreal>(V,W,n,t);
-
-    vector<vector<mpreal>> M = convert_to_wang_ball<mpreal>(f,beta,n);
-    mpreal wangBall = rationalWangBall<mpreal>(M,n,t);
-
-    cout << "DeCasteljau:      " << deCast << endl;
-    cout << "FarinDeCasteljau: " << farin << endl;
-    cout << "VS:               " << vs << endl;
-    cout << "HornBez:          " << hornBez << endl;
-    cout << "Lader:            " << lader << endl;
-    cout << "linearGeometric:  " << linearGeo << endl;
-    cout << "barycentric:      " << bary << endl;
-    cout << "WangBall:         " << wangBall << endl;
-
+    return 1;
 }
